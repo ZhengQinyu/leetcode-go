e345fab4253966485b87677cdc962a2d2da15923
zqyu <quincy125@qq.com>
2019-09-05 10:08:54 +0800

    回文数

---
+++ src/top/izqy/leetcode/code_9_test.go
@@ -0,0 +1,16 @@
+package leetcode
+
+import "testing"
+
+func TestIsPalindrome(t *testing.T) {
+
+	if isPalindrome(121) {
+		t.Log("ok")
+	}
+	if isPalindrome(-121) {
+		t.Fail()
+	}
+	if isPalindrome(10) {
+		t.Fail()
+	}
+}
