6a508d5096460fb8b2e4647ab3a5efa1e24d37a5
zqyu <quincy125@qq.com>
2019-09-06 18:30:59 +0800

    53

---
+++ src/top/izqy/leetcode/code_5.go
@@ -0,0 +1,13 @@
+package leetcode
+
+// f[i,j] = f[i+1, j-1] and si == sj
+
+func longestPalindrome(s string) string {
+	length := len(s)
+	//var result = make([]int, length*2)
+	for i := 0; i < length; i++ {
+		for j := 0; j < length; j++ {
+		}
+	}
+	return ""
+}
---
+++ src/top/izqy/leetcode/code_53_test.go
@@ -0,0 +1,17 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func TestMaxSubArray(t *testing.T) {
+	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	if x := maxSubArray(nums); x != 6 {
+		t.Fail()
+	}
+
+	nums = []int{-2}
+	if x := maxSubArray(nums); x != -2 {
+		t.Fail()
+	}
+}
