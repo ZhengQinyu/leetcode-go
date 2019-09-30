8219a97f9a43993dd5e8daad60388b5f6a58052c
zqyu <quincy125@qq.com>
2019-09-30 18:32:59 +0800

    添加解题方法

---
+++ src/top/izqy/leetcode/_20190930/code_693.go
@@ -0,0 +1,16 @@
+package _20190930
+
+/* 693. 交替位二进制数 */
+
+func hasAlternatingBits(n int) bool {
+	t := n % 2
+	for n >>= 1; n > 0; n >>= 1 {
+		k := n % 2
+		if t == k {
+			return false
+		}
+		t = k
+		n /= 2
+	}
+	return true
+}
