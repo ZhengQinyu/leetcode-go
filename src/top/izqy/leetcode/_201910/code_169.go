7a13ef70de88cbe3935849824f8ac6ca334ddded
zqyu <quincy125@qq.com>
2019-10-22 17:52:32 +0800

    70, 169, 268, 728

---
+++ src/top/izqy/leetcode/_201910/code_169.go
@@ -0,0 +1,18 @@
+package _201910
+
+/* 169. 求众数 */
+func majorityElement(nums []int) int {
+	m := make(map[int]int)
+	s := len(nums) / 2
+	for _, n := range nums {
+		if _, ok := m[n]; ok {
+			m[n]++
+		} else {
+			m[n] = 1
+		}
+		if m[n] > s {
+			return n
+		}
+	}
+	return nums[0]
+}
