59b94922f1fb164353b75e3f0940a0e54ec2c9de
zqyu <quincy125@qq.com>
2021-04-09 18:10:04 +0800

    save

---
+++ _202104/code_482.go
@@ -0,0 +1,19 @@
+package _202104
+
+import "bytes"
+
+// 482. 密钥格式化 未解决
+func licenseKeyFormatting(S string, K int) string {
+	var buffer bytes.Buffer
+	for _, ch := range S {
+		if ch >= 'a' && ch <= 'z' {
+			buffer.WriteByte(byte(ch - 32))
+		} else {
+			if ch == '-' {
+				continue
+			}
+			buffer.WriteByte(byte(ch))
+		}
+	}
+	//length := len(buffer.Bytes())
+}
