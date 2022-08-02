72cd6308033145087fad0b61e191ffdccfe445bd
zqyu <quincy125@qq.com>
2022-08-02 10:51:19 +0800

    2.两数相加

--- _202104/code_482.go
+++ _202104/code_482.go
@@ -5,6 +5,13 @@ import "bytes"
 // 482. 密钥格式化 未解决
 func licenseKeyFormatting(S string, K int) string {
 	var buffer bytes.Buffer
+	count := 0
+	for _, ch := range S {
+		if ch != '-' {
+			count++
+		}
+	}
+	count %= K
 	for _, ch := range S {
 		if ch >= 'a' && ch <= 'z' {
 			buffer.WriteByte(byte(ch - 32))
@@ -16,4 +23,5 @@ func licenseKeyFormatting(S string, K int) string {
 		}
 	}
 	//length := len(buffer.Bytes())
+	return ""
 }
